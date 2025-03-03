2fd4a5d379982a46c16e7ec0c74a9a74c8053eee
Atlas-Lofton <gll4392@Hotmail.com>
2025-03-03 17:40:29 -0600

    added javascript for tier 1.

---
+++ javascript/tier1.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let rate = document.getElementById("rating");
+let button = document.getElementById("button");
+let retry = document.getElementById("retry");
+
+function hideButton() {
+    button.style.display= none;
+}
+hideButton();
